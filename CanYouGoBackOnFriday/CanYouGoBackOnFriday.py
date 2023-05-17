e61925e7fe386b1ca8db8bba761d00c43ee50f3e
sabrinafongg <miyoko_raikkonen@yahoo.com>
2023-05-17 14:57:43 +0800

    Added Wednesday and Thursday Input..

--- CanYouGoBackOnFriday/CanYouGoBackOnFriday.py
+++ CanYouGoBackOnFriday/CanYouGoBackOnFriday.py
@@ -8,8 +8,15 @@ mondayout = input("Input your Monday Time Out:\n")
 tuesdayin = input("Input your Tuesday Time In:\n")
 tuesdayout = input("Input your Tuesday Time Out:\n")
 
+#Wednesday
+wednesdayin = input("Input your Wednesday Time In:\n")
+wednesdayout = input("Input your Wednesday Time Out:\n") 
 
-print("Monday In/Out:" + mondayin + " / " + mondayout + "\n" + "Tuesday In/Out:" + tuesdayin + " / " + tuesdayout  )
+#Thursday 
+thursdayin = input("Input your Thursday Time In:\n")
+thursdayout = input("Input your Thursday Time Out:\n") 
+
+#print("Monday In/Out:" + mondayin + " / " + mondayout + "\n" + "Tuesday In/Out:" + tuesdayin + " / " + tuesdayout  )
 
 # convert time string to datetime
 m1 = datetime.strptime(mondayin, "%H:%M")
@@ -18,12 +25,18 @@ m2 = datetime.strptime(mondayout, "%H:%M") + timedelta(hours=12)
 t1 = datetime.strptime(tuesdayin, "%H:%M")
 t2 = datetime.strptime(tuesdayout, "%H:%M") + timedelta(hours=12)
 
+w1 = datetime.strptime(wednesdayin, "%H:%M")
+w2 = datetime.strptime(wednesdayout, "%H:%M") + timedelta(hours=12)
+
+th1 = datetime.strptime(thursdayin, "%H:%M")
+th2 = datetime.strptime(thursdayout, "%H:%M") + timedelta(hours=12)
+
 # get difference
-delta = (m2 - m1) + (t2 - t1)
+delta = (m2 - m1) + (t2 - t1) + (w2 - w1) + (th2 - th1)
 
 #print(f"Time difference is {delta.total_seconds()} seconds")
 deltahour = delta.total_seconds() //3600
-deltahourleft = 28.5 - deltahour
+deltahourleft = 47.5 - deltahour
 
 print(f"You have clocked in a total of {deltahour} hours from Mon-Thur and {deltahourleft} hours left for Friday")
 
